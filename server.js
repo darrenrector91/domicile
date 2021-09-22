b9d6299f60a0c8d11b8e9de57e690bdefbed4afe
Darren <dgrector55428@gmail.com>
2021-09-21 20:34:53 -0500

    added dotenv config

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 app.get('/',(req,res) => res.send('App is running'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.REACT_APP_PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
